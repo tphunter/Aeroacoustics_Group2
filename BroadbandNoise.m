768035edc6c02aa19e9e97f6e29be5025ab46330
Pedro García González <pedro.garcia.gonzalez97@gmail.com>
2019-09-28 18:30:14 +0200

    Change

---
+++ BroadbandNoise.m
@@ -0,0 +1,14 @@
+%% TRAILING EDGE
+k_bar=param.omega*param.chord/(2*a_0);
+sigma=sqrt(param.x^2+(1-param.Mach^2)*(param.y^2+param.z^2));
+k_y_bar=k_y*param.cho/2
+I_TE_1=i*e^(2*i*C)/C*();
+I_TE_2=;
+
+I_TE=I_TE_1+I_TE_2;
+
+
+Spp_TE=();
+
+%% LEADING EDGE
+
