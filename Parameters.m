cffb3fd6a7f0d785a017d8dc9621e5ee7db2f161
ThomasH <thomashunterfr@gmail.com>
2019-09-28 18:29:28 +0200

    Added Param

--- Parameters.m
+++ Parameters.m
@@ -1,6 +1,15 @@
 %% Parameters
+%add all lengths in [m]
+%all angles in [rad]
+%all time in [s]
+%all mass in [kg]
+
 param = struct ; 
 
 
 %% 
 param.x = 0.1;
+param.cl = [];
+param.cd = [];
+param.sres = 50;
+param.phi = 0;
