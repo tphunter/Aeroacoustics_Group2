396435104270d678b412e97d85d1593c32f7145d
ThomasH <thomashunterfr@gmail.com>
2019-10-02 15:14:25 +0200

    Update Tonal_noise.m

--- Tonal_noise.m
+++ Tonal_noise.m
@@ -10,5 +10,5 @@ for s=-s:1:s
     Omegas = m*B*Omega/(m*B-s);
     F_s = F_s[i];
     P_mBi = i*m*B^2*Omega/(4*pi*c*R_0)*sum_-s^s*F_s*exp(-i*(m*B-s)*pi/2)*exp(i*(m*B-s)*(phi-Omega_s*R_0/c))*JmB-s(m*B*M*sin(theta))(-(m*B-s)/(m*B)*sin(gamma)/M+cos(theta)*cos(gamma));
-    p_mB %append
+    p_mB = P_mB * P_mBi;
 end
