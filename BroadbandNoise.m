2fd6c3557980ec7362601a3c04c8a6cd02314c68
Miguel Angel <miansaor@gmail.com>
2019-10-07 12:00:20 +0200

    Changes in Broadband (n)

--- BroadbandNoise.m
+++ BroadbandNoise.m
@@ -1,32 +1,33 @@
 p = Parameters();
 
 
-for i=1:p.sections
+for n=1:p.sections
 %% TRAILING EDGE
 
 %1.- Radiation integral
-k_bar(i) = p.omega*p.chord(i)/(2*p.c);
-epsilon(i)=1/sqrt(1+1/(4*k_bar(i)));
+k_bar(n) = p.omega*p.chord(n)/(2*p.c);
+epsilon(n)=1/sqrt(1+1/(4*k_bar(n)));
 sigma=sqrt(p.x^2+(1-p.Mach^2)*(p.y^2+p.z^2));
-k_bar_y(i)=p.k_y*p.chord(i)/2; %k_y might be a vector
-k_bar_x(i)=p.k_x*p.chord(i)/2; %k_x might be a vector
-mu_bar(i)=p.omega*p.chord(i)/(2*p.c*p.beta^2); %beta might be a vector 
-B(i)=k_bar_x(i)-p.Mach*mu_bar(i)+k_bar(i);
-C(i)=k_bar_x(i)-mu_bar(i)*(p.x/sigma-p.Mach);
-theta(i)=k_bar(i)-mu_bar(i)*p.x/sigma;
+k_bar_y(n)=p.k_y*p.chord(n)/2; %k_y might be a vector
+k_bar_x(n)=p.k_x*p.chord(n)/2; %k_x might be a vector
+mu_bar(n)=p.omega*p.chord(n)/(2*p.c*p.beta^2); %beta might be a vector 
+B(n)=k_bar_x(n)-p.Mach*mu_bar(n)+k_bar(n);
+C(n)=k_bar_x(n)-mu_bar(n)*(p.x/sigma-p.Mach);
+theta(n)=k_bar(n)-mu_bar(n)*p.x/sigma;
 
-alpha(i)=p.V/p.U_c; %p.U_c might be a vector (for sure)
-H(i)=((1+1i)*exp(-4*1i*k_bar(i))*(1-theta(i).^2))/(2*sqrt(pi)*...
-    (alpha(i)-1)*k_x*sqrt(B(i)));%k_x might be a vector
-G(i)=(1+epsilon(i))*exp(1i*(2*k_bar(i)+theta(i)))*sin(theta(i)-2*k_bar(i))/...
-    (theta(i)-2*k_bar(i))+(1-epsilon(i))*exp(1i*(-2*k_bar+theta))*...
-    sin(theta+2*k_bar)/(theta+2*k_bar)+(1+epsilon)*(1-1i)/...
-    (2*(theta-2*k_bar))*exp(4*1i*k_bar)*fresnelc(4*k_bar)-(1-epsilon)...
-    *(1+1i)/(2*(theta+2*k_bar))*exp(4*1i*k_bar)*fresnelc(4*k_bar)...
-    +exp(2*1i*theta)/2*sqrt(2*k_bar/theta)*fresnelc(2*theta)*...
-    ((1-epsilon)*(1+1i)/(theta+2*k_bar)-(1+epsilon)*(1+1i)/(theta-2*k_bar)); %I use Fresnel cosine func
-I_TE_1=1i*e^(2*1i*C)/C*((1+1i)*exp(-2*1i*C)*sqrt(B/(B-C))*fresnel(2*B-2*C)-(1+1i)*fresnel(2*B)+1); %I use the Fresnel integral of cosine, ask if we should use the sine one.
-I_TE_2=H*(exp(4*1i*k_bar)*(1-(1+1i)*fresnel(4*k_bar)))^epsilon+H(exp(-2*1i*theta)+1i*(theta+k_bar_x+p.Mach*mu_bar-k_bar)*G); %Take a look at definition of epsilon (slide 8 lecture 4), it should not be an exponent.
+alpha(n)=p.V/p.U_c; %p.U_c might be a vector (for sure)
+H(n)=((1+1i).*exp(-4*1i.*k_bar(n))*(1-theta(n).^2))./(2*sqrt(pi).*...
+    (alpha(n)-1).*p.k_x.*sqrt(B(n)));%k_x might be a vector
+G(n)=(1+epsilon(n)).*exp(1i.*(2.*k_bar(n)+theta(n))).*sin(theta(n)-2.*k_bar(n))./...
+    (theta(n)-2.*k_bar(n))+(1-epsilon(n)).*exp(1i.*(-2.*k_bar(n)+theta(n))).*...
+    sin(theta(n)+2.*k_bar(n))./(theta(n)+2.*k_bar(n))+(1+epsilon(n)).*(1-1i)./...
+    (2.*(theta(n)-2.*k_bar(n))).*exp(4.*1i.*k_bar(n)).*fresnel(4.*k_bar(n))-(1-epsilon(n))...
+    .*(1+1i)./(2.*(theta(n)+2.*k_bar(n))).*exp(4.*1i.*k_bar(n)).*fresnel(4.*k_bar(n))...
+    +exp(2.*1i.*theta(n))/2.*sqrt(2.*k_bar(n)./theta(n)).*fresnel(2.*theta(n)).*...
+    ((1-epsilon(n)).*(1+1i)./(theta(n)+2.*k_bar(n))-(1+epsilon(n)).*(1+1i)./(theta(n)-2.*k_bar(n))); %I use Fresnel cosine func
+
+I_TE_1=1i*exp(2*1i*C(n))./C(n)*((1+1i)*exp(-2*1i*C(n))*sqrt(B(n)/(B(n)-C(n)))*fresnel(2*B(n)-2*C(n))-(1+1i)*fresnel(2*B(n))+1); %I use the Fresnel integral of cosine, ask if we should use the sine one.
+I_TE_2=H*(exp(4*1i*k_bar(n))*(1-(1+1i)*fresnel(4*k_bar(n))))^epsilon(n)+H(n).*(exp(-2*1i*theta(n))+1i*(theta(n)+k_bar_x(n)+p.Mach*mu_bar(n)-k_bar(n))*G); %Take a look at definition of epsilon (slide 8 lecture 4), it should not be an exponent.
 
 I_TE=I_TE_1+I_TE_2;
 
@@ -39,7 +40,7 @@ Re_T_SS=p.u_tau*p.delta_SS*sqrt(p.C_f/2)/p.kinvis;
 beta_c_SS=theta/p.tau_w_SS*p.dP_dx_SS;
 fun_PI_SS=@(PI_SS_var) 2*PI_SS_var-log(1+PI_SS_var)-kappa*p.V/...
     p.u_tau-log(p.deltastar_SS*p.V/p.kinvis)-0.51*kappa-log(kappa);
-PI_SS=fzero(fun_PI_SS,1);
+PI_SS = fzero(fun_PI_SS,1);
 phi_pp_omega_SS=(p.tau_w_SS^2*p.delta_star_SS/(p.V)*0.78*...
     (1.8*PI_SS*beta_c_SS+6)*(p.omega*p.delta_star_SS/p.V)^2)/...
     (((p.omega*p.delta_star_SS/p.V)^0.75+0.105)^3.7+...
@@ -86,7 +87,7 @@ L_t=0.4*p.turb_kin_en^1.5/p.diss_rate;
 u_bar_prime=sqrt(2*p.turb_kin_en/3);
 k_e=sqrt(pi)/L_t*gamma(5/6)/gamma(1/3);
 phi_omegaomega_omega = u_bar_prime^2*L_t/(2*pi*p.V)*...
-    (1+8/3*(p.k_x+k_e)^2)/((1+(p.k_x/k_e)^2)^(211/6));
+    (1+8/3*(p.k_x+k_e)^2)/((1+(p.k_x/k_e)^2).^(211/6));
 l_y_LE=(gamma(1/3)/gamma(5/6))^2*(p.k_x/k_e)^2/...
     ((3+8*(p.k_x+k_e)^2)*sqrt(1+(p.k_x+k_e)^2));
 
@@ -101,8 +102,8 @@ end
 %% SUBFUNCTIONS
 function[E] = fresnel(x)
 
-fun = @(t) (exp(-1i*t))/(sqrt(2*PI_SS*t));
-E = integral(fun,0,x);
+fun = @(t) (exp(-1i*t))/(sqrt(2*pi*t));
+E = integral(fun,0,x,'ArrayValued',1);
 
 end
 
