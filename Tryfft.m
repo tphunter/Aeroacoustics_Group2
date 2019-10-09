8ade5c50333b0b62e745ccb18d567885a230f642
ThomasH <thomashunterfr@gmail.com>
2019-10-09 14:38:11 +0200

    It might actually work

--- Tryfft.m
+++ Tryfft.m
@@ -1,5 +1,5 @@
 t = 0:1/50:10-1/50;                     
-x = sin(2*pi*15*t) + sin(2*pi*20*t);
+x = sin(2*pi*15*t) + sin(2*pi*20*t)+1;
 plot(t,x)
 y = fft(x);     
 f = (0:length(y)-1)*50/length(y);
