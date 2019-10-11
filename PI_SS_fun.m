29edc0d2ec1c24bb51a4270962aa92463ea129b7
Miguel Angel <miansaor@gmail.com>
2019-10-11 11:56:01 +0200

    Three more...

---
+++ PI_SS_fun.m
@@ -0,0 +1,7 @@
+function[PI_SS] = PI_SS_fun(p,n)
+
+Karman = 0.41;
+
+PI_SS=@(PI_SS_var) 2.*PI_SS_var-log(1+PI_SS_var)-Karman.*p.V./...
+    p.u_tau-log(p.deltastar_SS.*p.V./p.kinvis)-0.51.*Karman-log(Karman);
+end
